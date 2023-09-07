f387f5d4300499ed5eb45fee22aa4080826ac9b8
AlmogHaviv <almoghaviv1310@gmail.com>
2023-09-07 12:03:10 +0300

    creating new utils in pdf and new utils in excel

--- src/pdf/mini-jobs/scraping-txt/scraping_txt.py
+++ src/pdf/mini-jobs/scraping-txt/scraping_txt.py
@@ -6,7 +6,21 @@ import PyPDF4
 
 import pandas as pd
 
+import os
+
+import glob
+
 #create a function
+def find_all_pdf_in_directory():
+    script_path = os.path.abspath(__file__)
+    script_directory = os.path.dirname(script_path)
+    print("Script Directory:", script_directory)
+    pdf_files = glob.glob(os.path.join(script_directory, '*.pdf'))
+    pdf_filenames = [os.path.basename(pdf_file) for pdf_file in pdf_files]
+    print(pdf_filenames)
+    return pdf_filenames
+
+
 def getPageCount(filepath):
     file = open(filepath, 'rb')
     # creating a pdf reader object
@@ -17,43 +31,38 @@ def getPageCount(filepath):
     return totalpages
 
 
-def extract_book_names_from_pdf(pdf_file):
-    num_of_pages = getPageCount(pdf_file)
-    # Read the PDF file
-    pdf = pdfplumber.open(pdf_file)
-
-    # Define the regex pattern to match book names
-    pattern = r'([A-Z]+\d+)\s(.+)'  # Match a capital letter(s) followed by one or more digits
-
+def extract_book_names_from_pdf(list_of_files):
     # Create a list to store the book names
     book_names = []
-
-    for page_num in range(num_of_pages):
-        page = pdf.pages[page_num]
-        text = page.extract_text()
-        print(text)
-        matches = re.findall(pattern, text)
-        for match in matches:
-            book_code, book_name = match
-            final_book_name = ""
-            word_list = book_name.split()
-            counter = 0
-            while (word_list[counter].isupper()) and (counter + 1 < len(word_list)):
-                final_book_name += word_list[counter] + " "
-                counter += 1
-            print("Book Code:", book_code)
-            print("Book Name:", book_name)
-            print()
-            book_names.append((book_code, final_book_name[:-1]))
-
     
-    return book_names
+    # Define the regex pattern to match book names
+    pattern = r'([A-Z]+\d+)\s+([A-Z\s,()]+)\s+([\s\S]+?)(?=[A-Z]+\d+|$)'
+
+    for pdf_file in list_of_files:
+        num_of_pages = getPageCount(pdf_file)
+        # Read the PDF file
+        pdf = pdfplumber.open(pdf_file)
+
+        for page_num in range(num_of_pages):
+            page = pdf.pages[page_num]
+            text = page.extract_text()
+            print(text)
+            matches = re.findall(pattern, text)
+            for match in matches:
+                book_code, book_name ,book_info = match
+                print("Book Code:", book_code)
+                print("Book Name:", book_name)
+                print()
+                book_names.append(match)
+
+    final_book_names = sorted(book_names)
+    return final_book_names
 
 
-def convert_to_excel(filename):
-    ful_data = extract_book_names_from_pdf(filename)
+def convert_to_excel(list_of_files):
+    ful_data = extract_book_names_from_pdf(list_of_files)
     # Convert the list of tuples to a DataFrame
-    df = pd.DataFrame(ful_data, columns=['Index', 'Book Name'])
+    df = pd.DataFrame(ful_data, columns=['Index', 'Book Name', 'Book Info'])
 
     # Create an Excel writer object
     writer = pd.ExcelWriter('book-names-lists.xlsx', engine='xlsxwriter')
@@ -66,5 +75,5 @@ def convert_to_excel(filename):
     print("Excel file 'output.xlsx' created successfully!")
 
 # Test the function
-convert_to_excel('sample-pdf.pdf')
-
+list_of_files = find_all_pdf_in_directory()
+extract_book_names_from_pdf(list_of_files)
--- src/pdf/mini-jobs/scraping-txt/testim.py
+++ src/pdf/mini-jobs/scraping-txt/testim.py
@@ -1,3 +1,41 @@
-line = "HELLP"
+import re
 
-print(line[:-1])
+def extract_company_data(file_path):
+    # Create a list to store the captured data
+    captured_data = []
+
+    # Define the regex pattern
+    pattern = r'([A-Z]+\d+)\s+([\w\s,()]+)'
+
+    # Read the .txt file
+    with open(file_path, 'r', encoding='utf-8') as file:
+        capturing = False
+        company_data = []
+
+        for line in file:
+            # Check if the line matches the pattern for company data
+            match = re.match(pattern, line.strip())
+            
+            if match:
+                # Capture the company data and set capturing flag to True
+                company_code, company_name = match.groups()
+                company_data.append((company_code, company_name))
+                print(company_code, company_name)
+                capturing = True
+            elif capturing:
+                # Add the line to the captured company data
+                company_data.append(line.strip())
+            else:
+                # If not capturing, continue to the next line
+                continue
+
+        # Append the captured company data to the list
+        if company_data:
+            captured_data.append(company_data)
+
+    return captured_data
+
+# Example usage:
+data = extract_company_data('extracted_text.txt')
+for i in range(4):
+    print(data[i])
